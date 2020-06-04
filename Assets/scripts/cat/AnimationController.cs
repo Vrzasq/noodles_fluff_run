4aa73b72faae391ae6493f25b83859cf1c2e09fc
Vrzasq <ridzio1986@hotmail.com>
2020-06-04 20:24:27 +0200

    Reimport assets

---
+++ Assets/scripts/cat/AnimationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private Transform parentTransorm;
+    void Awake()
+    {
+        rigidBody = GetComponentInParent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentPosition = new Vector2(rigidBody.position.x, rigidBody.position.y);
+        rigidBody.MovePosition(currentPosition + (Vector2.right * Time.deltaTime));
+    }
+}
