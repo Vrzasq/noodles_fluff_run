2d95d1aef4ae65e6b3312448798d754c19f19817
Vrzasq <ridzio1986@hotmail.com>
2020-06-09 01:27:17 +0200

    Add character touch control

--- Assets/scripts/cat/AnimationController.cs
+++ Assets/scripts/cat/AnimationController.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using UnityEditor.Animations;
+﻿using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
@@ -9,26 +8,41 @@ public class AnimationController : MonoBehaviour
     [SerializeField] Rigidbody2D rigidbody2D;
     SpriteRenderer spriteRenderer;
 
-    void Awake()
+    void Start()
     {
         if (rigidbody2D == null)
-            rigidbody2D = GetComponentInParent<Rigidbody2D>();
-            
+            throw new ArgumentNullException(nameof(rigidbody2D));
+
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        FlipSprite();
+        SetSpriteOrientation();
+    }
+
+    void FixedUpdate()
+    {
         animator.SetFloat("speed", Mathf.Abs(rigidbody2D.velocity.x));
     }
 
-    void FlipSprite()
+    void SetSpriteOrientation()
     {
         if (rigidbody2D.velocity.x < 0)
             spriteRenderer.flipX = true;
-        else
+        else if (rigidbody2D.velocity.x > 0)
             spriteRenderer.flipX = false;
     }
+
+    public void OnJump()
+    {
+        animator.SetBool("isJumping", true);
+    }
+
+    public void OnLand()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
 }
--- Assets/scripts/cat/CatMovement.cs
+++ Assets/scripts/cat/CatMovement.cs
@@ -1,27 +1,99 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace noodles_fluff_run.Assets.scripts.cat
 {
     public class CatMovement : MonoBehaviour
     {
-        public float force = 7f;
-        public Vector2 direction = Vector2.right;
+        [SerializeField] float moveForce = 7f;
+        [SerializeField] float maxSpeed = 3.5f;
+        [SerializeField] float jumpForce = 15f;
+        [SerializeField] Vector2 direction = Vector2.right;
+        [SerializeField] Transform groundCheck;
+        [SerializeField] private LayerMask groundMask;
+
         Rigidbody2D rigidbody2D;
 
+        private bool shouldJump = false;
+        private bool isGrounded = true;
+        private Vector2 velocitySnapShot = Vector2.zero;
 
-        void Awake()
+        public UnityEvent Jump;
+        public UnityEvent Land;
+
+        private void Awake()
         {
             rigidbody2D = GetComponent<Rigidbody2D>();
+            RegisterSwipes();
         }
 
-        void Update()
+        private void FixedUpdate()
         {
+            MoveCat();
+            CheckGround();
 
+            if (CanJump())
+                MakeJump();
         }
 
-        void FixedUpdate()
+        private void MoveCat()
         {
-            rigidbody2D.AddForce(direction * force);
+            if (Mathf.Abs(rigidbody2D.velocity.x) < maxSpeed)
+                rigidbody2D.AddForce(direction * moveForce);
         }
+
+
+        private void MakeJump()
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            shouldJump = false;
+            isGrounded = false;
+            Jump?.Invoke();
+        }
+
+        private bool CanJump() => shouldJump && isGrounded;
+
+        private void CheckGround()
+        {
+            Debug.Log(isGrounded);
+            if (isGrounded)
+                return;
+
+            if (Physics2D.OverlapBox(groundCheck.position, new Vector2(0.4f, 0.1f), 0, groundMask) != null)
+            {
+                isGrounded = true;
+                Land?.Invoke();
+            }
+        }
+
+        private void RegisterSwipes()
+        {
+            SwipeEvents.OnSwipeLeft += OnSwipeLeft;
+            SwipeEvents.OnSwipeRight += OnSwipRight;
+            SwipeManager.OnSingleTap += OnSingleTap;
+        }
+
+        private void OnSingleTap()
+        {
+            shouldJump = true;
+        }
+
+        private void OnSwipeLeft()
+        {
+            if (direction == Vector2.right)
+                direction = Vector2.zero;
+            else if (direction == Vector2.zero)
+                direction = Vector2.left;
+        }
+
+        private void OnSwipRight()
+        {
+            if (direction == Vector2.left)
+                direction = Vector2.zero;
+            else if (direction == Vector2.zero)
+                direction = Vector2.right;
+        }
+
     }
 }
