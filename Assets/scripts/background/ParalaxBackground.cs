4efc5a6f252b0389e7eaf2da38aaa5f5688c2a48
Vrzasq <jakub.romel@gmail.com>
2020-06-21 15:36:25 +0200

    add background

---
+++ Assets/scripts/background/ParalaxBackground.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class ParalaxBackground : MonoBehaviour
+{
+    [SerializeField] Transform cameraTransform;
+    [SerializeField] float paralaxEffect = 0.5f;
+
+    private Vector3 lastCameraPosition;
+
+    void Start()
+    {
+        if (cameraTransform == null)
+            throw new NullReferenceException($"{nameof(cameraTransform)} is null");
+
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += deltaMovement * paralaxEffect;
+        lastCameraPosition = cameraTransform.position;
+    }
+
+}
